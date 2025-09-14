4d4dadf32ee4b0ce2800ecb944f146d1f03a4793
fyteclubplugin <fyteclubplugin@gmail.com>
2025-09-14 12:50:51 -0400

    Clean up root directory - remove redundant docs and scripts

--- copy-plugin.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Copying FyteClub plugin files...
-copy "c:\Users\Me\git\fyteclub\plugin\bin\Release\FyteClub.dll" "C:\Users\Me\AppData\Roaming\XIVLauncher\installedPlugins\FyteClub\4.0.0\"
-copy "c:\Users\Me\git\fyteclub\plugin\bin\Release\FyteClub.deps.json" "C:\Users\Me\AppData\Roaming\XIVLauncher\installedPlugins\FyteClub\4.0.0\"
-copy "c:\Users\Me\git\fyteclub\plugin\bin\Release\Penumbra.Api.dll" "C:\Users\Me\AppData\Roaming\XIVLauncher\installedPlugins\FyteClub\4.0.0\"
-copy "c:\Users\Me\git\fyteclub\plugin\bin\Release\Glamourer.Api.dll" "C:\Users\Me\AppData\Roaming\XIVLauncher\installedPlugins\FyteClub\4.0.0\"
-echo Plugin files copied successfully!
-echo Please restart FFXIV completely to load the updated plugin.
-pause
